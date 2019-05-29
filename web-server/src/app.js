33b3b9a6eb7e0b17a1123ae2e1dc2d862f3a83dd
Quentin Guenther <qguenther@outlook.com>
2019-05-29 00:31:26 -0700

    create basic express web server for waether app

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,19 +1,64 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
-const publicDirectoryPath = path.join(__dirname, '../public')
 
+// Define paths for express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup Handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
-    res.send('<h1>Hello Express</h1>')
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Quentin Guenther'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Quentin Guenther'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'FAQ (...and other Questions)',
+        title: 'Help',
+        name: 'Quentin Guenther'
+    })
 })
 
 app.get('/weather', (req, res) => {
     res.send({
         forecast: 'Its 50 degrees',
         location: 'Seattle, Washington'
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        message: 'Help article not found!',
+        name: 'Quentin Guenther'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        message: 'Page not found!',
+        name: 'Quentin Guenther'
     })
 })
 

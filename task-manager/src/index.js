da015a29c400973581e5f0d953769dd46c9a39f6
Quentin Guenther <qguenther@outlook.com>
2019-06-30 23:01:21 -0700

    authentication and logging out

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -6,8 +6,19 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(express.json())
+// app.use((req, res, next) => {
+//   if(req.method === 'GET') {
+//     res.send('Get requests are disabled')
+//   } else {
+//     next()
+//   }
+// })
 
+// app.use((req, res, next) => {
+//   res.status(503).send('The site is under maintenance temporarily')
+// })
+
+app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
@@ -15,14 +26,14 @@ app.listen(port, () => {
   console.log('Server is up on port:', port)
 })
 
-const jwt = require('jsonwebtoken')
+// const jwt = require('jsonwebtoken')
 
-const myFunction = async () => {
-  const token = jwt.sign({ _id: 'abc123' }, 'this is my new course', { expiresIn: '0 second'})
-  console.log(token)
+// const myFunction = async () => {
+//   const token = jwt.sign({ _id: 'abc123' }, 'this is my new course', { expiresIn: '0 second'})
+//   console.log(token)
 
-  const data = jwt.verify(token, 'this is my new course')
-  console.log(data)
-}
+//   const data = jwt.verify(token, 'this is my new course')
+//   console.log(data)
+// }
 
-myFunction();
+// myFunction();

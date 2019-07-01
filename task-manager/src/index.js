dc384279d4568e466bec984f651ba69f95658808
Quentin Guenther <qguenther@outlook.com>
2019-06-30 23:30:15 -0700

    hide user data and add authentication to user routes

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -25,15 +25,3 @@ app.use(taskRouter)
 app.listen(port, () => {
   console.log('Server is up on port:', port)
 })
-
-// const jwt = require('jsonwebtoken')
-
-// const myFunction = async () => {
-//   const token = jwt.sign({ _id: 'abc123' }, 'this is my new course', { expiresIn: '0 second'})
-//   console.log(token)
-
-//   const data = jwt.verify(token, 'this is my new course')
-//   console.log(data)
-// }
-
-// myFunction();

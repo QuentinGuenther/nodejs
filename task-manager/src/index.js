af2ee0f42e4f4eff21cab45deb86288d8c483fbe
Quentin Guenther <qguenther@outlook.com>
2019-07-02 00:44:44 -0700

    task - user relationship

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -25,3 +25,18 @@ app.use(taskRouter)
 app.listen(port, () => {
   console.log('Server is up on port:', port)
 })
+
+const Task = require('./models/task')
+const User = require('./models/user')
+
+const main = async () => {
+  // const task = await Task.findById("5d1b00e0d254d527e74ed357")
+  // await task.populate('owner').execPopulate()
+  // console.log(task)
+
+  // const user = await User.findById("5d1b00cad254d527e74ed355")
+  // await user.populate('tasks').execPopulate()
+  // console.log(user.tasks)
+}
+
+main()
--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const Task = require('./task')
 
 const userSchema = mongoose.Schema({
   name: {
@@ -49,6 +50,12 @@ const userSchema = mongoose.Schema({
   }]
 })
 
+userSchema.virtual('tasks', {
+  ref: 'Task',
+  localField: '_id',
+  foreignField: 'owner'
+})
+
 userSchema.methods.generateAuthToken = async function() {
   const user = this
   const token = jwt.sign({ _id: user._id.toString() }, 'thisismynewcourse')
@@ -90,6 +97,14 @@ userSchema.pre('save', async function(next) {
   if(user.isModified('password')) {
     user.password = await bcrypt.hash(user.password, 8)
   }
+
+  next()
+})
+
+userSchema.pre('remove', async function(next) {
+  const user = this
+
+  await Task.deleteMany({ owner: user._id })
 
   next()
 })

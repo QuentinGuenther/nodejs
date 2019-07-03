436062b809c1af297e08bbf824c276f93d83f5ea
Quentin Guenther <qguenther@outlook.com>
2019-07-02 19:40:21 -0700

    allow client to filter, paginate, and sort tasks

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -19,8 +19,28 @@ router.post('/tasks', auth, async (req, res) => {
 })
 
 router.get('/tasks', auth, async (req, res) => {
+  const match = {}
+  const sort = {}
+
+  if(req.query.sortBy) {
+    const parts = req.query.sortBy.split(':')
+    sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+  }
+
+  if(req.query.completed) {
+    match.completed = req.query.completed === 'true'
+  }
+
   try {
-    await req.user.populate('tasks').execPopulate()
+    await req.user.populate({
+      path: 'tasks',
+      match,
+      options: {
+        limit: parseInt(req.query.limit),
+        skip: parseInt(req.query.skip),
+        sort
+      }
+    }).execPopulate()
     const tasks = req.user.tasks
     res.send(tasks)
   } catch(e) {
